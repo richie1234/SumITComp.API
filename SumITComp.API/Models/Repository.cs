d2d87f5912cfeb085817f9c1998d39ded5ac1114
Ruchitha <ruchitha@outlook.com>
2016-03-24 09:22:15 +1100

    Repository file(error fixed)

---
+++ SumITComp.API/Models/Repository.cs
@@ -0,0 +1,77 @@
+﻿using System.Linq;
+using SumITComp.Repository.Entities;
+
+namespace SumITComp.API.Models
+{
+    public class Repository : IRepository
+    {
+        private SumITCompAPIContext _ctx;
+
+        public Repository(SumITCompAPIContext ctx)
+        {
+            this._ctx = ctx;
+        }
+
+
+        public IQueryable<Product> GetAllProducts()
+        {
+            return _ctx.Products;
+        }
+
+        public Product GetProduct(int id)
+        {
+            return _ctx.Products.Where(f => f.ProductId == id).FirstOrDefault(); ;
+        }
+
+        public bool AddProduct(Product product)
+        {
+            try
+            {
+                _ctx.Products.Add(product);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+        public bool SaveAll()
+        {
+            return _ctx.SaveChanges() > 0;
+        }
+
+        public bool DeleteProductEntry(int id)
+        {
+            try
+            {
+                var entity = _ctx.Products.Where(f => f.ProductId == id).FirstOrDefault();
+                if (entity != null)
+                {
+                    _ctx.Products.Remove(entity);
+                    return true;
+                }
+            }
+            catch
+            {
+                // TODO Logging
+            }
+
+            return false;
+        }
+
+        public bool UpdateProduct(Product product)
+        {
+            if (product != null)
+            {
+                _ctx.Products.Attach(product);
+                // _ctx.Products.Attach(new Product() {ProductId = 1,Title = "hello",Description = "Rambo",Price = 70});
+
+                _ctx.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+    }
+}
